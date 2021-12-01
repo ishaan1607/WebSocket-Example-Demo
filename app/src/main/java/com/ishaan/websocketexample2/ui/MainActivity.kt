f152e1f0b0041530549eb8d569adea2b00dd0ae1
Ishaan Jain <ishaan.jain@likeminds.community>
2021-12-01 11:37:06 +0530

    main fragment created with binding

--- app/src/main/java/com/ishaan/websocketexample2/ui/MainActivity.kt
+++ app/src/main/java/com/ishaan/websocketexample2/ui/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ishaan.websocketexample2.R
 import com.ishaan.websocketexample2.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
--- app/src/main/java/com/ishaan/websocketexample2/ui/MainFragment.kt
+++ app/src/main/java/com/ishaan/websocketexample2/ui/MainFragment.kt
@@ -1,4 +1,25 @@
 package com.ishaan.websocketexample2.ui
 
-class MainFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ishaan.websocketexample2.databinding.FragmentMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
 }
